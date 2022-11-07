3c5c2e3ecd0c9d67326c48e5804bdc37ab8d1223
Dean Nellis <dean@dotdash.io>
2022-11-07 16:06:45 -0500

    update ascii animation with theme change

--- src/sections/AsciiAnimation/index.tsx
+++ src/sections/AsciiAnimation/index.tsx
@@ -6,7 +6,11 @@ import Model from "./Model.jsx";
 
 const AsciiAnimation = () => {
     return (
-        <div style={{ backgroundColor: "#fff" }} className="ascii">
+        <div
+            style={{ backgroundColor: "#fff" }}
+            className="ascii"
+            title="Click and Drag to Rotate"
+        >
             <Canvas>
                 <Suspense fallback={null}>
                     <color attach="background" args={["black"]} />
@@ -44,7 +48,7 @@ const RotatingModel = () => {
                 onClick={() => click(!clicked)}
                 onPointerOver={() => hover(true)}
                 onPointerOut={() => hover(false)}
-                position={[1.75, -0.5, 0]}
+                position={[1.75, -1.25, 1]}
             >
                 <Model />
             </mesh>
@@ -59,21 +63,33 @@ function AsciiRenderer({
 }) {
     // Reactive state
     const { size, gl, scene, camera } = useThree();
+    const storedColorMode =
+        localStorage.getItem("themeIsLight") !== null
+            ? localStorage.getItem("themeIsLight") === "true"
+            : false;
 
     // Create effect
     const effect = useMemo(() => {
+        const themeIsLight = storedColorMode;
         const effect = new AsciiEffect(gl, characters, options);
+        if (themeIsLight) {
+            effect.domElement.style.color = "white";
+            effect.domElement.style.backgroundColor = "black";
+        } else {
+            effect.domElement.style.color = "black";
+            effect.domElement.style.backgroundColor = "white";
+        }
         effect.domElement.style.position = "absolute";
         effect.domElement.style.top = "0px";
         effect.domElement.style.left = "0px";
-        effect.domElement.style.color = "white";
-        effect.domElement.style.backgroundColor = "black";
         effect.domElement.style.pointerEvents = "none";
+        effect.domElement.style.cursor = "pointer";
         return effect;
     }, [characters, options.invert]);
 
     // Append on mount, remove on unmount
     useEffect(() => {
+        const themeIsLight = storedColorMode;
         gl.domElement.parentNode.appendChild(effect.domElement);
         return () => gl.domElement.parentNode.removeChild(effect.domElement);
     }, [effect]);
@@ -82,6 +98,17 @@ function AsciiRenderer({
     useEffect(() => {
         effect.setSize(size.width, size.height);
     }, [effect, size]);
+
+    useEffect(() => {
+        const themeIsLight = storedColorMode;
+        if (themeIsLight) {
+            effect.domElement.style.color = "white";
+            effect.domElement.style.backgroundColor = "black";
+        } else {
+            effect.domElement.style.color = "black";
+            effect.domElement.style.backgroundColor = "white";
+        }
+    }, [localStorage.getItem("themeIsLight")]);
 
     // Take over render-loop (that is what the index is for)
     useFrame((state) => {
