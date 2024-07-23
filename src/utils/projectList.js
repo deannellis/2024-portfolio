6586a49a65f70d237f0350358056c22b22e17570
Dean Nellis <dean@dotdash.io>
2024-07-23 09:24:43 -0400

    fix import

--- src/utils/projectList.js
+++ src/utils/projectList.js
@@ -1,12 +1,7 @@
-import flextCover from "../assets/images/flext_screencap_02.png";
-import gourmandCover from "../assets/images/Gourmand_Cover.png";
 import kineticTypeCover from "../assets/images/kineticType_Cover.png";
-import diceRollerCover from "../assets/images/diceRoller_Cover.png";
-import cobLifeCover from "../assets/images/CobLife_Cover.jpg";
-import ispCover from "../assets/images/ISP_Cover.jpg";
 import divertCover from "../assets/images/Divert_Cover.png";
 import kamiCover from "../assets/images/Kami_Cover.png";
-import noliCover from "../assets/images/Noli_Cover.png";
+import noliCover from "../assets/images/NOLI_Cover.png";
 import randomRulesCover from "../assets/images/RandomRules_Cover.png";
 
 export const projectList = [
